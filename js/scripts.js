788d1df921374b5026642bb0327c4c4438b0548c
Francois(Frank) Senechal <senf01@ymail.com>
2022-11-12 12:42:48 -0500

    workshop 4

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,13 @@ $(function() {
         }
     });
 });
+
+$(function() {
+    $('#reserveButton').click(function(){
+        $('#reserveModal').modal('show');
+    })    
+    
+    $('#loginButton').click(function (){
+        $('#loginModal').modal('show');
+    })    
+});
